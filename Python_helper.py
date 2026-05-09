bc9c383f535710eba9baa73f8a78a00cddcfe37b
komilovafzal37-maker <komilovafzal37@gmail.com>
2026-05-09 16:25:34 +0500

    Add dotenv support for environment variables

--- Python_helper.py
+++ Python_helper.py
@@ -1,5 +1,6 @@
 import asyncio
 import os   
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import (
     Message,
@@ -7,9 +8,14 @@ from aiogram.types import (
     KeyboardButton
 )   
 
-BOT_TOKEN = os.getenv("BOT_TOKEN") 
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 bot = Bot(token=BOT_TOKEN)
+
 dp = Dispatcher()
 
 lessons = {
